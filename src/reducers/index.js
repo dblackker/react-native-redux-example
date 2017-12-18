23fece1da1c75bec2d62d72bf8f07832f61438eb
Daniel Black <daniel.black@thit.com>
2017-12-18 17:30:11 -0600

    Init

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
+
+export default combineReducers({
+    libraries: LibraryReducer
+});
+
+
+// console.log(store.getState());
+// { libraries: [] }
